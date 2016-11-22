bb6e4338d1e2b94ccdaf61ec1bb2ebe6ad1c711a
Ryan Skraba <rskraba@talend.com>
2016-11-22 15:46:08 +0100

    fix: support custom record and field names for single column conversions.

--- daikon/src/test/java/org/talend/daikon/avro/converter/SingleColumnIndexedRecordConverterTest.java
+++ daikon/src/test/java/org/talend/daikon/avro/converter/SingleColumnIndexedRecordConverterTest.java
@@ -22,7 +22,7 @@ public class SingleColumnIndexedRecordConverterTest {
         assertThat(converter.getSchema().getType(), is(Schema.Type.RECORD));
         assertThat(converter.getSchema().getName(), is("byteArrayRecord"));
         assertThat(converter.getSchema().getFields(), hasSize(1));
-        assertThat(converter.getSchema().getFields().get(0).name(), is("field1"));
+        assertThat(converter.getSchema().getFields().get(0).name(), is("field"));
         assertThat(converter.getSchema().getFields().get(0).schema().getType(), is(Schema.Type.BYTES));
 
         // Check the results of the conversion.
@@ -30,6 +30,25 @@ public class SingleColumnIndexedRecordConverterTest {
         assertThat(converted.getSchema(), is(converter.getSchema()));
         assertThat(converted.get(0), instanceOf(byte[].class));
         assertArrayEquals(new byte[] { 1, 2, 3 }, (byte[]) converted.get(0));
+    }
+
+    @Test
+    public void testCustomRecordAndFieldNames() {
+        // Create a converter based on a String with custom names.
+        SingleColumnIndexedRecordConverter<String> converter = new SingleColumnIndexedRecordConverter<>(String.class,
+                Schema.create(Schema.Type.STRING), "MyRecord", "payload");
+        assertThat(converter.getDatumClass(), equalTo(String.class));
+        assertThat(converter.getSchema().getType(), is(Schema.Type.RECORD));
+        assertThat(converter.getSchema().getName(), is("MyRecord"));
+        assertThat(converter.getSchema().getFields(), hasSize(1));
+        assertThat(converter.getSchema().getFields().get(0).name(), is("payload"));
+        assertThat(converter.getSchema().getFields().get(0).schema().getType(), is(Schema.Type.STRING));
+
+        // Check the results of the conversion.
+        IndexedRecord converted = converter.convertToAvro("123");
+        assertThat(converted.getSchema(), is(converter.getSchema()));
+        assertThat(converted.get(0), instanceOf(String.class));
+        assertThat((String) converted.get(0), is("123"));
     }
 
 }
