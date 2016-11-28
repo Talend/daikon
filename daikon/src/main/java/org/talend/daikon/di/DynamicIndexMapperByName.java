b5d69be97e1937be3f4d4b94fe087b463e73cf16
Ivan Honchar <ivan.honchar@globallogic.com>
2016-11-28 22:23:02 +0200

    fix(TDI-37866): add dynamic field position property check

--- daikon/src/main/java/org/talend/daikon/di/DynamicIndexMapperByName.java
+++ daikon/src/main/java/org/talend/daikon/di/DynamicIndexMapperByName.java
@@ -54,8 +54,9 @@ class DynamicIndexMapperByName implements DynamicIndexMapper {
         this.designSchema = designSchema;
         this.designFields = designSchema.getFields();
         this.designSchemaSize = designFields.size();
-        if (AvroUtils.isIncludeAllFields(designSchema)) {
-            dynamicFieldPosition = Integer.valueOf(designSchema.getProp(DiSchemaConstants.TALEND6_DYNAMIC_COLUMN_POSITION));
+        String dynamicFieldProperty = designSchema.getProp(DiSchemaConstants.TALEND6_DYNAMIC_COLUMN_POSITION);
+        if (AvroUtils.isIncludeAllFields(designSchema) && dynamicFieldProperty != null) {
+            dynamicFieldPosition = Integer.valueOf(dynamicFieldProperty);
         } else {
             throw new IllegalArgumentException("Runtime schema doesn't contain dynamic field");
         }
