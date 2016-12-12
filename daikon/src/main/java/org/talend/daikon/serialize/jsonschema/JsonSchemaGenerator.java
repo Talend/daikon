2d7a0bead73abe9db76c8036573908185afc808b
francisu <fupton@talend.com>
2016-12-12 15:56:40 +0100

    TDKN-110 Add CITIZEN_USER form and preferred Form (javadoc)

--- daikon/src/main/java/org/talend/daikon/serialize/jsonschema/JsonSchemaGenerator.java
+++ daikon/src/main/java/org/talend/daikon/serialize/jsonschema/JsonSchemaGenerator.java
@@ -8,6 +8,10 @@ import static org.talend.daikon.serialize.jsonschema.JsonBaseTool.isListClass;
 import java.util.Date;
 import java.util.List;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.talend.daikon.NamedThing;
 import org.talend.daikon.properties.Properties;
 import org.talend.daikon.properties.ReferenceProperties;
@@ -16,17 +20,12 @@ import org.talend.daikon.properties.property.EnumListProperty;
 import org.talend.daikon.properties.property.EnumProperty;
 import org.talend.daikon.properties.property.Property;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.node.ArrayNode;
-import com.fasterxml.jackson.databind.node.JsonNodeFactory;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-
 /**
  * Provide methods to create JSON Schema documents from a {@link Properties}.
- *
+ * <p>
  * This JSON Schema can be used to validate a Properties in its "reduced JSON" format as provided by
  * {@link JsonDataGenerator}.
- *
+ * <p>
  * <ul>
  * <li>https://spacetelescope.github.io/understanding-json-schema/</li>
  * </ul>
@@ -35,6 +34,7 @@ public class JsonSchemaGenerator {
 
     /**
      * @param properties the properties to create a JSON Schema representation for.
+     * @param formName   the formName to use to get the title for the form.
      * @return the JSON Schema representation.
      */
     protected ObjectNode genSchema(Properties properties, String formName) {
@@ -43,7 +43,10 @@ public class JsonSchemaGenerator {
 
     private ObjectNode processTProperties(Properties cProperties, String formName) {
         ObjectNode schema = JsonNodeFactory.instance.objectNode();
-        Form form = cProperties.getPreferredForm(formName);
+        Form form = null;
+        if (formName != null) {
+            form = cProperties.getPreferredForm(formName);
+        }
         if (form != null) {
             schema.put(JsonSchemaConstants.TAG_TITLE, form.getTitle());
         }
@@ -109,8 +112,8 @@ public class JsonSchemaGenerator {
             schema.put(JsonSchemaConstants.TAG_TYPE, JsonSchemaConstants.getTypeMapping().get(property.getType()));
             if (Date.class.getName().equals(property.getType())) {
                 schema.put(JsonSchemaConstants.TAG_FORMAT, "date-time");// Do not support other format for date till
-                                                                        // Property
-                                                                        // support it
+                // Property
+                // support it
             }
         }
         return schema;
