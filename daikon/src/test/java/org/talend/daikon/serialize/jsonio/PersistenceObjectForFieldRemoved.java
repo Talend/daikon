618d5b519f41f6ebb8e37d0a708671711cfc60ac
Sebastien Gandon <sgandon@talend.com>
2017-02-27 13:39:01 +0100

    fix(migration):fix codacy errors

--- daikon/src/test/java/org/talend/daikon/serialize/jsonio/PersistenceObjectForFieldRemoved.java
+++ daikon/src/test/java/org/talend/daikon/serialize/jsonio/PersistenceObjectForFieldRemoved.java
@@ -4,25 +4,25 @@ import org.talend.daikon.serialize.DeserializeDeletedFieldHandler;
 
 public class PersistenceObjectForFieldRemoved implements DeserializeDeletedFieldHandler {
 
+    public String persObjStr = "persObjStrValue";
+
+    public InnerClass1 in1 = new InnerClass1();
+
+    public Object in2;// we don't use Inner2 to have an @type in the serialization
+
+    public boolean isdeletedFieldIn2_right_type;
+
     public static class InnerClass1 {
 
-        String inner1Str = "inner1Str";
+        public String inner1Str = "inner1Str";
     }
 
     public static class InnerClass2 {
 
-        String inner2Str = "inner2Str";
+        public String inner2Str = "inner2Str";
 
-        InnerClass1 in1inIn2;
+        public InnerClass1 in1inIn2;
     }
-
-    String persObjStr = "persObjStrValue";
-
-    InnerClass1 in1 = new InnerClass1();
-
-    Object in2;// we don't use Inner2 to have an @type in the serialization
-
-    public boolean isdeletedFieldIn2_right_type;
 
     public PersistenceObjectForFieldRemoved() {
         InnerClass2 aIn2 = new InnerClass2();
@@ -33,7 +33,7 @@ public class PersistenceObjectForFieldRemoved implements DeserializeDeletedField
     @Override
     public boolean deletedField(String fieldName, Object value) {
         boolean migrated = false;
-        if (fieldName.equals("in2Old")) {
+        if ("in2Old".equals(fieldName)) {
             isdeletedFieldIn2_right_type = (value != null && (value instanceof InnerClass2));
             migrated = true;
         }
