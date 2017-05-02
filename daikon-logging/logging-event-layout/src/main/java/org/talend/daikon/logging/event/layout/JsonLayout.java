ede25545195c8fde2b678cb42c1c6fd993e75e60
sdiallo-talend <sdiallo@talend.com>
2017-05-02 11:52:30 +0200

    Take in account codacy-bot request changes

--- daikon-logging/logging-event-layout/src/main/java/org/talend/daikon/logging/event/layout/JsonLayout.java
+++ daikon-logging/logging-event-layout/src/main/java/org/talend/daikon/logging/event/layout/JsonLayout.java
@@ -3,11 +3,6 @@ package org.talend.daikon.logging.event.layout;
 import ch.qos.logback.core.LayoutBase;
 import ch.qos.logback.core.spi.DeferredProcessingAware;
 
-public class JsonLayout<Event extends DeferredProcessingAware> extends LayoutBase<Event> {
-
-    @Override
-    public String doLayout(Event event) {
-        return null;
-    }
+public abstract class JsonLayout<E extends DeferredProcessingAware> extends LayoutBase<E> {
 
 }
