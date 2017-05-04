3a145c332887ef801a6b7bb077aef853b7aedc51
sdiallo-talend <sdiallo@talend.com>
2017-05-04 15:51:18 +0200

    Request Changes from Daikon Team

--- daikon-logging/logging-event-layout/src/test/java/org/talend/daikon/logging/layout/Log4j2JSONLayoutTest.java
+++ daikon-logging/logging-event-layout/src/test/java/org/talend/daikon/logging/layout/Log4j2JSONLayoutTest.java
@@ -129,32 +129,4 @@ public class Log4j2JSONLayoutTest {
         return LayoutFields.DATETIME_TIME_FORMAT.format(timestamp);
     }
 
-    /*@Test
-    public void testThrowableFull() {
-        final String[] options = { "full" };
-        final ThrowablePatternConverter converter = ThrowablePatternConverter.newInstance(options);
-        Throwable parent;
-        try {
-            try {
-                throw new NullPointerException("null pointer");
-            } catch (final NullPointerException e) {
-                throw new IllegalArgumentException("IllegalArgument", e);
-            }
-        } catch (final IllegalArgumentException e) {
-            parent = e;
-        }
-        final LogEvent event = Log4jLogEvent.newBuilder() //
-                .setLoggerName("testLogger") //
-                .setLoggerFqcn(this.getClass().getName()) //
-                .setLevel(Level.DEBUG) //
-                .setMessage(new SimpleMessage("test exception")) //
-                .setThrown(parent).build();
-        final StringBuilder sb = new StringBuilder();
-        converter.format(event, sb);
-        final String result = sb.toString();
-        //System.out.print(result);
-        assertTrue("Incorrect start of msg", result.startsWith("java.lang.IllegalArgumentException: IllegalArgument"));
-        assertTrue("Missing nested exception", result.contains("java.lang.NullPointerException: null pointer"));
-    }*/
-
 }
