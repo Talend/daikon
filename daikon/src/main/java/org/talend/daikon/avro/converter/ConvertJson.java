27d989fc94204f49ffe3418b005e2ef6122ca8d4
Zakaria AFKIR <zafkir@talend.com>
2017-07-11 15:49:14 +0200

    feat(TFD-1713): Create converter json to avro schema

---
+++ daikon/src/main/java/org/talend/daikon/avro/converter/ConvertJson.java
@@ -0,0 +1,178 @@
+package org.talend.daikon.avro.converter;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.JsonNodeType;
+import com.fasterxml.jackson.databind.node.NullNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.apache.avro.Schema;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Random;
+
+public class ConvertJson implements AvroConverter<String, Schema> {
+
+    private static final Logger logger = LoggerFactory.getLogger(AvroConverter.class);
+
+    private static final String NAME = "name";
+
+    private static final String TYPE = "type";
+
+    private static final String ARRAY = "array";
+
+    private static final String ITEMS = "items";
+
+    private static final String STRING = "string";
+
+    private static final String RECORD = "record";
+
+    private static final String FIELDS = "fields";
+
+    private static final String NULL = "null";
+
+    private final ObjectMapper mapper;
+
+    private Schema outputSchema = null;
+
+    /**
+     * Constructor
+     *
+     * @param mapper
+     */
+    public ConvertJson(final ObjectMapper mapper) {
+        this.mapper = mapper;
+    }
+
+    @Override
+    public Schema getSchema() {
+        return null;
+    }
+
+    @Override
+    public Class<String> getDatumClass() {
+        return null;
+    }
+
+    @Override
+    public String convertToDatum(Schema value) {
+        return null;
+    }
+
+    @Override
+    public Schema convertToAvro(String value) {
+        try {
+            final JsonNode jsonNode = mapper.readTree(value);
+            final ObjectNode finalSchema = mapper.createObjectNode();
+            finalSchema.put("namespace", "org.talend");
+            finalSchema.put(NAME, "outer_record");
+            finalSchema.put(TYPE, RECORD);
+            finalSchema.set(FIELDS, getFields(jsonNode));
+            outputSchema = new Schema.Parser().parse(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(finalSchema));
+        } catch(Exception ex) {
+            logger.error(ex.getMessage());
+        }
+        return outputSchema;
+    }
+
+    /**
+     * Get fields of json node.
+     *
+     * @param jsonNode
+     * @return fields of json node
+     */
+    public ArrayNode getFields(final JsonNode jsonNode) {
+        final ArrayNode fields = mapper.createArrayNode();
+        final Iterator<Map.Entry<String, JsonNode>> elements = jsonNode.fields();
+
+        Map.Entry<String, JsonNode> map;
+        while (elements.hasNext()) {
+            map = elements.next();
+            final JsonNode nextNode = map.getValue();
+            ObjectNode fieldNode = mapper.createObjectNode();
+            ArrayNode fieldTypeArray = mapper.createArrayNode();
+
+            if (!(nextNode instanceof NullNode)) {
+                switch (nextNode.getNodeType()) {
+                    case NUMBER:
+                        fieldNode.put(NAME, map.getKey());
+                        fieldTypeArray.add(NULL);
+                        if (nextNode.isInt()) {
+                            fieldTypeArray.add("int");
+                        } else if (nextNode.isLong()) {
+                            fieldTypeArray.add("long");
+                        } else {
+                            fieldTypeArray.add("double");
+                        }
+                        fieldNode.put(TYPE, fieldTypeArray);
+                        fields.add(fieldNode);
+                        break;
+
+                    case STRING:
+                        fieldNode.put(NAME, map.getKey());
+                        fieldTypeArray.add(NULL);
+                        fieldTypeArray.add(STRING);
+                        fieldNode.put(TYPE, fieldTypeArray);
+                        fields.add(fieldNode);
+                        break;
+
+                    case ARRAY:
+                        final ArrayNode arrayNode = (ArrayNode) nextNode;
+                        final JsonNode element = arrayNode.get(0);
+                        final ObjectNode objectNode = mapper.createObjectNode();
+                        objectNode.put(NAME, map.getKey());
+
+                        if (element.getNodeType() == JsonNodeType.NUMBER) {
+                            fieldNode.put(TYPE, ARRAY);
+                            fieldTypeArray.add(NULL);
+                            fieldTypeArray.add((nextNode.get(0).isLong() ? "long" : "double"));
+                            fieldNode.put(ITEMS, fieldTypeArray);
+                            objectNode.set(TYPE, fieldNode);
+                        } else if (element.getNodeType() == JsonNodeType.STRING) {
+                            fieldNode.put(TYPE, ARRAY);
+                            fieldTypeArray.add(NULL);
+                            fieldTypeArray.add(STRING);
+                            fieldNode.put(ITEMS, fieldTypeArray);
+                            objectNode.set(TYPE, fieldNode);
+                        } else {
+                            objectNode.set(TYPE, mapper.createObjectNode().put(TYPE, ARRAY).set(ITEMS, mapper.createObjectNode()
+                                    .put(TYPE, RECORD).put(NAME, generateRandomNumber(map)).set(FIELDS, getFields(element))));
+                        }
+                        fields.add(objectNode);
+                        break;
+
+                    case OBJECT:
+                        ObjectNode node = mapper.createObjectNode();
+                        node.put(NAME, map.getKey());
+                        node.set(TYPE, mapper.createObjectNode().put(TYPE, RECORD).put(NAME, generateRandomNumber(map)).set(FIELDS,
+                                getFields(nextNode)));
+                        fields.add(node);
+                        break;
+
+                    default:
+                        logger.error("Node type not found - " + nextNode.getNodeType());
+                        throw new RuntimeException("Unable to determine action for node type " + nextNode.getNodeType()
+                                + "; Allowed types are ARRAY, STRING, NUMBER, OBJECT");
+                }
+            } else {
+                fieldNode.put(NAME, map.getKey());
+                fieldTypeArray.add(NULL);
+                fieldTypeArray.add(STRING);
+                fieldNode.put(TYPE, fieldTypeArray);
+                fields.add(fieldNode);
+            }
+        }
+        return fields;
+    }
+
+    /**
+     * Generate random.
+     *
+     * @param map to create random number
+     */
+    public String generateRandomNumber(Map.Entry<String, JsonNode> map) {
+        return (map.getKey() + "_" + new Random().nextInt(100));
+    }
+}
---
+++ daikon/src/test/java/org/talend/daikon/avro/converter/ConvertJsonTest.java
@@ -0,0 +1,259 @@
+package org.talend.daikon.avro.converter;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import org.apache.avro.Schema;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.AbstractMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class ConvertJsonTest {
+
+    private final ConvertJson convertJson = new ConvertJson(new ObjectMapper());
+
+    private final String simpleJson = "{\"a\": {\"b\": \"b1\"}, \"d\": \"d1\"}";
+
+    private final String arrayJson = "{\"a\": [{\"b\": \"b1\"}, {\"b\": \"b2\"}]}";
+
+    private final String nullJson = "{\"a\": null}";
+
+    private final String intJson = "{\"a\": 100}";
+
+    private final String doubleJson = "{\"a\": 100.1}";
+
+    /**
+     * Get fields of the input record: {@link ConvertJsonTest#simpleJson}
+     *
+     * Expected fields:
+     * [{"name":"a","type":{"type":"record","name":"a_49","fields":[{"name":"b","type":["null","string"]}]}},{"name":"d","type":["null","string"]}]
+     *
+     * @throws IOException
+     */
+    @Test
+    public void testGetFields_simpleJson() throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode jsonNode = mapper.readTree(simpleJson);
+
+        ArrayNode arrayNode = convertJson.getFields(jsonNode);
+        assertEquals(2, arrayNode.size());
+
+        // Check `a` field
+        JsonNode nodeA = arrayNode.get(0);
+
+        // "name":"a"
+        assertEquals("a", nodeA.get("name").textValue());
+
+        // "type":{"type":"record","name":"a_49","fields":[{"name":"b","type":["null","string"]}]}
+        JsonNode nodeAType = nodeA.get("type");
+
+        // type":"record"
+        JsonNode nodeATypeRecord = nodeAType.get("type");
+        assertEquals("record", nodeATypeRecord.textValue());
+
+        // "fields":[{"name":"b","type":["null","string"]}]
+        ArrayNode nodeATypeFields = (ArrayNode) nodeAType.get("fields");
+
+        // {"name":"b","type":["null","string"]}
+        JsonNode nodeB = nodeATypeFields.get(0);
+
+        // "name":"b"
+        assertEquals("b", nodeB.get("name").textValue());
+
+        // "type":["null","string"]
+        ArrayNode nodeBType = (ArrayNode) nodeB.get("type");
+        assertEquals("null", nodeBType.get(0).textValue());
+        assertEquals("string", nodeBType.get(1).textValue());
+
+        // Check `d` field
+        JsonNode nodeD = arrayNode.get(1);
+
+        // "name":"d"
+        assertEquals("d", nodeD.get("name").textValue());
+
+        // "type":["null","string"]
+        ArrayNode nodeDType = (ArrayNode) nodeD.get("type");
+        assertEquals("null", nodeDType.get(0).textValue());
+        assertEquals("string", nodeDType.get(1).textValue());
+    }
+
+    /**
+     * Get fields of the input record: {@link ConvertJsonTest#arrayJson}
+     *
+     * Expected fields:
+     * [{"name":"a","type":{"type":"array","items":{"type":"record","name":"a_35","fields":[{"name":"b","type":["null","string"]}]}}}]
+     *
+     * @throws IOException
+     */
+    @Test
+    public void testGetFields_arrayJson() throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode jsonNode = mapper.readTree(arrayJson);
+
+        ArrayNode arrayNode = convertJson.getFields(jsonNode);
+        assertEquals(1, arrayNode.size());
+
+        // Check `a` field
+        JsonNode nodeA = arrayNode.get(0);
+
+        // "name":"a"
+        assertEquals("a", nodeA.get("name").textValue());
+
+        // "type":{"type":"array","items":{"type":"record","name":"a_35","fields":[{"name":"b","type":["null","string"]}]}}
+        JsonNode nodeAType = nodeA.get("type");
+
+        // type":"array"
+        JsonNode nodeATypeArray = nodeAType.get("type");
+        assertEquals("array", nodeATypeArray.textValue());
+
+        // "items":{"type":"record","name":"a_35","fields":[{"name":"b","type":["null","string"]}]}
+        JsonNode nodeATypeItems = nodeAType.get("items");
+
+        // "fields":[{"name":"b","type":["null","string"]}
+        ArrayNode nodeAFields = (ArrayNode) nodeATypeItems.get("fields");
+
+        JsonNode nodeB = nodeAFields.get(0);
+
+        // "name":"b"
+        assertEquals("b", nodeB.get("name").textValue());
+
+        // "type":["null","string"]
+        ArrayNode nodeBType = (ArrayNode) nodeB.get("type");
+        assertEquals("null", nodeBType.get(0).textValue());
+        assertEquals("string", nodeBType.get(1).textValue());
+    }
+
+    /**
+     * Get fields of the input record: {@link ConvertJsonTest#nullJson}
+     *
+     * Expected fields:
+     * [{"name":"a","type":["null","string"]}]
+     *
+     * @throws IOException
+     */
+    @Test
+    public void testGetFields_nullJson() throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode jsonNode = mapper.readTree(nullJson);
+
+        ArrayNode arrayNode = convertJson.getFields(jsonNode);
+        assertEquals(1, arrayNode.size());
+
+        // Check `a` field
+        JsonNode nodeA = arrayNode.get(0);
+
+        // "name":"a"
+        assertEquals("a", nodeA.get("name").textValue());
+
+        // "type":["null","string"]
+        ArrayNode nodeAType = (ArrayNode) nodeA.get("type");
+        assertEquals("null", nodeAType.get(0).textValue());
+        assertEquals("string", nodeAType.get(1).textValue());
+    }
+
+    /**
+     * Get fields of the input record: {@link ConvertJsonTest#intJson}
+     *
+     * Expected fields:
+     * [{"name":"a","type":["null","int"]}]
+     *
+     * @throws IOException
+     */
+    @Test
+    public void testGetFields_intJson() throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode jsonNode = mapper.readTree(intJson);
+
+        ArrayNode arrayNode = convertJson.getFields(jsonNode);
+        assertEquals(1, arrayNode.size());
+
+        // Check `a` field
+        JsonNode nodeA = arrayNode.get(0);
+
+        // "name":"a"
+        assertEquals("a", nodeA.get("name").textValue());
+
+        // "type":["null","int"]
+        ArrayNode nodeAType = (ArrayNode) nodeA.get("type");
+        assertEquals("null", nodeAType.get(0).textValue());
+        assertEquals("int", nodeAType.get(1).textValue());
+    }
+
+    /**
+     * Get fields of the input record: {@link ConvertJsonTest#doubleJson}
+     *
+     * Expected fields:
+     * [{"name":"a","type":["null","double"]}]
+     *
+     * @throws IOException
+     */
+    @Test
+    public void testGetFields_doubleJson() throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode jsonNode = mapper.readTree(doubleJson);
+
+        ArrayNode arrayNode = convertJson.getFields(jsonNode);
+        assertEquals(1, arrayNode.size());
+
+        // Check `a` field
+        JsonNode nodeA = arrayNode.get(0);
+
+        // "name":"a"
+        assertEquals("a", nodeA.get("name").textValue());
+
+        // "type":["null","double"]
+        ArrayNode nodeAType = (ArrayNode) nodeA.get("type");
+        assertEquals("null", nodeAType.get(0).textValue());
+        assertEquals("double", nodeAType.get(1).textValue());
+    }
+
+    /**
+     * Convert the input record: {@link ConvertJsonTest#simpleJson} to avro schema
+     *
+     * Expected avro schema:
+     * {"type":"record","name":"outer_record","namespace":"org.talend",
+     * "fields":[{"name":"a","type":{"type":"record","name":"a_98","fields":[{"name":"b","type":["null","string"]}]}},
+     * {"name":"d","type":["null","string"]}]}
+     *
+     * @throws IOException
+     */
+    @Test
+    public void testConvertToAvroSchema() throws IOException {
+
+        Schema schema = convertJson.convertToAvro(simpleJson);
+        List<Schema.Field> fields = schema.getFields();
+        assertEquals(2, fields.size());
+
+        // Get `a` field
+        Schema.Field fieldA = fields.get(0);
+        Schema schemaA = fieldA.schema();
+        List<Schema.Field> fieldsA = schemaA.getFields();
+
+        // Check the schema of `b` field
+        Schema.Field fieldB = fieldsA.get(0);
+        Schema schemaB = fieldB.schema();
+        List<Schema> typesB = schemaB.getTypes();
+        assertEquals("null", typesB.get(0).getName());
+        assertEquals("string", typesB.get(1).getName());
+
+        // Check the schema of `d` field
+        Schema.Field fieldD = fields.get(1);
+        Schema schemaD = fieldD.schema();
+        List<Schema> typesD = schemaD.getTypes();
+        assertEquals("null", typesD.get(0).getName());
+        assertEquals("string", typesD.get(1).getName());
+    }
+
+    @Test
+    public void testGenerateRandomNumber() {
+        Map.Entry<String, JsonNode> entry = new AbstractMap.SimpleEntry<String, JsonNode>("a", null);
+        String randomNumber = convertJson.generateRandomNumber(entry);
+        assertNotNull(randomNumber);
+    }
+}
