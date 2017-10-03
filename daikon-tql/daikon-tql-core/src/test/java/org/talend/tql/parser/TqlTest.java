96372697bc1b928f1dc49775a6f5f210f485dc2b
Henri-Mayeul de Benque <hmdebenque@talend.com>
2017-10-03 10:13:58 +0200

    fix: Codacy review applied

--- daikon-tql/daikon-tql-core/src/test/java/org/talend/tql/parser/TqlTest.java
+++ daikon-tql/daikon-tql-core/src/test/java/org/talend/tql/parser/TqlTest.java
@@ -2,6 +2,10 @@ package org.talend.tql.parser;
 
 import org.junit.Test;
 import org.talend.tql.excp.TqlException;
+import org.talend.tql.model.Expression;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
 
 public class TqlTest {
 
@@ -9,16 +13,19 @@ public class TqlTest {
     public void parse() throws Exception {
         String query = "toto = 'hello world'";
 
-        Tql.parse(query);
+        Expression parse = Tql.parse(query);
 
         // no exception
+        assertNotNull(parse);
     }
 
     @Test(expected = TqlException.class)
-    public void parse_element() throws Exception {
+    public void parse_elementThrowTqlException() throws Exception {
         String query = "toto";
 
         Tql.parse(query);
+
+        fail(); // should have thrown exception
     }
 
 }
