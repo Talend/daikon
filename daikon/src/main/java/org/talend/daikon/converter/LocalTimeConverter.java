40743e01d3a8458b27239f2bca7ad99f7f15f4dd
BOURGEOIS Laurent <lbourgeois@talend.com>
2017-10-16 12:22:22 +0200

    TFD-1696 : LocalTime and LocalDateTime converters throw talendRuntimeException when parse fails

--- daikon/src/main/java/org/talend/daikon/converter/LocalTimeConverter.java
+++ daikon/src/main/java/org/talend/daikon/converter/LocalTimeConverter.java
@@ -1,7 +1,10 @@
 package org.talend.daikon.converter;
 
+import org.talend.daikon.exception.TalendRuntimeException;
+
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 public class LocalTimeConverter extends Converter<LocalTime> {
 
@@ -9,11 +12,22 @@ public class LocalTimeConverter extends Converter<LocalTime> {
 
     @Override
     public LocalTime convert(Object value) {
+        if (properties.containsKey(LocalDateTimeConverter.FORMATTER)) {
+            try {
+                return LocalTime.parse(value.toString(), getDateTimeFormatter());
+            } catch (DateTimeParseException dtpe) {
+                throw TalendRuntimeException.createUnexpectedException("Unable to parse " + value.toString());
+            }
+        }
         return LocalTime.parse(value.toString());
     }
 
     public LocalTimeConverter withDateTimeFormatter(DateTimeFormatter formatter) {
         properties.put(LocalTimeConverter.FORMATTER, formatter);
         return this;
+    }
+
+    public DateTimeFormatter getDateTimeFormatter() {
+        return (DateTimeFormatter) properties.get(LocalDateTimeConverter.FORMATTER);
     }
 }
