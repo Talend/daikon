2b51304cd42b7e6c4108746e6c20c943c141fc7e
sdiallo-talend <sdiallo@talend.com>
2018-01-05 16:42:01 +0100

    Remove unsed imports

--- daikon-logging/logging-request-interceptor/src/main/java/org/talend/daikon/logging/TalendFeignRequestInterceptor.java
+++ daikon-logging/logging-request-interceptor/src/main/java/org/talend/daikon/logging/TalendFeignRequestInterceptor.java
@@ -5,11 +5,6 @@ import java.io.UnsupportedEncodingException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpRequest;
-import org.springframework.http.client.ClientHttpRequestExecution;
-import org.springframework.http.client.ClientHttpRequestInterceptor;
-import org.springframework.http.client.ClientHttpResponse;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import feign.Request;
