6db2520a464e41921e95cc8158775fe784941331
Ryan Skraba <rskraba@talend.com>
2018-02-27 09:11:05 +0100

    fix: Convert java.time objects directly into String.

--- daikon/src/main/java/org/talend/daikon/converter/StringConverter.java
+++ daikon/src/main/java/org/talend/daikon/converter/StringConverter.java
@@ -21,18 +21,21 @@ public class StringConverter extends WithFormatConverter<StringConverter, String
             if (value instanceof Number) {
                 return LocalDate.ofEpochDay(((Number) value).longValue()).format(getDateFormatter());
             } else if (value instanceof LocalDate) {
+                return ((LocalDate) value).format(getDateFormatter());
             }
         } else if (getTimeMillisFormatter() != null) {
             if (value instanceof Number) {
                 return Instant.ofEpochMilli(((Number) value).intValue()).atZone(ZoneOffset.ofTotalSeconds(0)).toLocalTime()
                         .format(getTimeMillisFormatter());
             } else if (value instanceof LocalTime) {
+                return ((LocalTime) value).format(getTimeMillisFormatter());
             }
         } else if (getTimestampMillisFormatter() != null) {
             if (value instanceof Number) {
                 return Instant.ofEpochMilli(((Number) value).intValue()).atZone(ZoneOffset.ofTotalSeconds(0)).toLocalDateTime()
                         .format(getTimestampMillisFormatter());
             } else if (value instanceof LocalDateTime) {
+                return ((LocalDateTime) value).format(getTimestampMillisFormatter());
             }
         }
         return value.toString();
