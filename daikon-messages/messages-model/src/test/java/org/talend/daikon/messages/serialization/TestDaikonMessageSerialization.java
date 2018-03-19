33496d6f69e931742a3052350151f4b26db22567
fabballe <fabballe@talend.com>
2018-03-19 10:52:07 +0100

    chore(): Add TU asked from review

---
+++ daikon-messages/messages-model/src/test/java/org/talend/daikon/messages/serialization/TestDaikonMessageSerialization.java
@@ -0,0 +1,36 @@
+package org.talend.daikon.messages.serialization;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.talend.daikon.messages.MessageKey;
+
+public class TestDaikonMessageSerialization {
+
+    private DaikonMessageKeyDeserializer daikonMessageKeyDeserializer = new DaikonMessageKeyDeserializer();
+
+    private DaikonMessageKeySerializer daikonMessageKeySerializer = new DaikonMessageKeySerializer();
+
+    @Test
+    public void testSerializationDeserialisation() {
+        Map<String, String> keys = new HashMap<>();
+        keys.put("key1", "value1");
+        keys.put("key2", "value2");
+
+        MessageKey data = MessageKey.newBuilder().setRandom("random1234").setTenantId("tenant1").setKeys(keys).build();
+        byte[] serializeData = daikonMessageKeySerializer.serialize("topic", data);
+
+        MessageKey deserializeData = daikonMessageKeyDeserializer.deserialize("topic", serializeData);
+
+        asserMessageKey(data, deserializeData);
+
+    }
+
+    private void asserMessageKey(MessageKey expectedMessageKey, MessageKey currentMessageKey) {
+        Assert.assertEquals(expectedMessageKey.getTenantId(), currentMessageKey.getTenantId());
+        Assert.assertEquals(expectedMessageKey.getRandom(), currentMessageKey.getRandom());
+        Assert.assertEquals(expectedMessageKey.getKeys(), currentMessageKey.getKeys());
+    }
+}
