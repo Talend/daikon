b42149f428424e97e9bb02ef1f313a3f1d93737f
Damien Prot <dprot@talend.com>
2018-09-04 17:47:31 +0200

    feat(TDQ-15761): Translate word patterns into mongo/regex patterns

---
+++ daikon/src/main/java/org/talend/daikon/pattern/word/WordPatternToRegexConstants.java
@@ -0,0 +1,27 @@
+package org.talend.daikon.pattern.word;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class WordPatternToRegexConstants {
+
+    // Iso to Character.isDigit
+    public static final String DIGIT = "\\p{Nd}";
+
+    // Iso to Character.isLetter
+    public static final String LETTER = "\\p{L}";
+
+    // Iso to Character.isIdeographic
+    public static final String IDEOGRAM = "\\p{InHangul_Jamo}|\\p{InHangul_Compatibility_Jamo}|\\p{InHangul_Syllables}|\\p{script=Han}";
+
+    public static final String CHAR = WordPatternToRegexConstants.LETTER + "&&[^" + IDEOGRAM + "]"; // Except ideograms
+
+    // Iso to Character.isUpperCase
+    public static final String UPPER_CHAR = "\\p{Lu}";
+
+    public static final String LOWER_CHAR = CHAR + "&&[^" // Except uppers because only lower won't contain insensitive letters
+            + WordPatternToRegexConstants.UPPER_CHAR + "]";
+
+    public static final List<Character> ESCAPE_CHARACTERS = Arrays.asList('(', ')', '[', ']', '.', '?', '+', '*');
+
+}
