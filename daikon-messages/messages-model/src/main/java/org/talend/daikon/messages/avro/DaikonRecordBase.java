2892d065936cdd200596cc29683c7ffe1672b044
fhuaulme <fhuaulme@talend.com>
2018-11-09 22:39:12 +0100

    fix(TDKN-205): Prevent using Avro default toString
    * Prevent use of unfiltered toString() in generated classes.
    * Add custom templates to change record super class with custom one.

---
+++ daikon-messages/messages-model/src/main/java/org/talend/daikon/messages/avro/DaikonRecordBase.java
@@ -0,0 +1,9 @@
+package org.talend.daikon.messages.avro;
+
+public abstract class DaikonRecordBase extends org.apache.avro.specific.SpecificRecordBase {
+
+    @Override
+    public String toString() {
+        return "DaikonRecordBase{}";
+    }
+}
