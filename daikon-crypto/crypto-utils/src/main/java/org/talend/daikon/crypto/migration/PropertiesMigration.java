b4c2f8972973547eef603196802815aa93223a7b
fhuaulme <fhuaulme@talend.com>
2019-08-12 19:07:14 +0200

    * clean up. * add readme section about migrations.

--- daikon-crypto/crypto-utils/src/main/java/org/talend/daikon/crypto/migration/PropertiesMigration.java
+++ daikon-crypto/crypto-utils/src/main/java/org/talend/daikon/crypto/migration/PropertiesMigration.java
@@ -2,9 +2,18 @@ package org.talend.daikon.crypto.migration;
 
 import java.util.Set;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.talend.daikon.crypto.PropertiesEncryption;
 
+/**
+ * Provide a migration helper for property files.
+ *
+ * @see PropertiesEncryption
+ */
 public class PropertiesMigration {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(PropertiesMigration.class);
 
     private final EncryptionMigration migration;
 
@@ -14,6 +23,13 @@ public class PropertiesMigration {
 
     private final PropertiesEncryption propertiesEncryption;
 
+    /**
+     * Builds a migration helper for property files.
+     *
+     * @param migration The {@link EncryptionMigration} to handle value migrations.
+     * @param input The property file name (with an absolute path).
+     * @param propertyNames The names of the properties to migrate.
+     */
     public PropertiesMigration(EncryptionMigration migration, String input, Set<String> propertyNames) {
         this.migration = migration;
         this.input = input;
@@ -22,11 +38,17 @@ public class PropertiesMigration {
         propertiesEncryption = new PropertiesEncryption(migration.getTarget());
     }
 
+    /**
+     * Performs the migration of the <code>input</code> property file.
+     *
+     * @see EncryptionMigration#migrate(String)
+     */
     public void migrate() {
-        propertiesEncryption.encryptAndSave(this.input, this.propertyNames, s -> {
+        propertiesEncryption.encryptAndSave(input, propertyNames, s -> {
             try {
                 return migration.migrate(s);
             } catch (Exception e) {
+                LOGGER.error("Unable to migrate input '{}' (keep previous value).", s);
                 return s;
             }
         });
