cd4f48e3b7e45e386640d119ca86fbc75a0a393d
fhuaulme <fhuaulme@talend.com>
2020-07-09 10:37:37 +0200

    fix(TDKN-nnn): Fix number comparison when using "="
    * Fix BeanPredicateVisitor predicate creation when using "=" with a numeric value.
    * Add unit test for issue.

--- daikon-tql/daikon-tql-bean/src/test/java/org/talend/tql/bean/BeanPredicateVisitorTest.java
+++ daikon-tql/daikon-tql-bean/src/test/java/org/talend/tql/bean/BeanPredicateVisitorTest.java
@@ -484,6 +484,18 @@ public class BeanPredicateVisitorTest {
         assertFalse(predicate.test(bean));
     }
 
+    @Test
+    public void testDoubleIntComparison() {
+        // given
+        final Expression query = Tql.parse("int = 10.0");
+
+        // when
+        final Predicate<Bean> predicate = query.accept(new BeanPredicateVisitor<>(Bean.class));
+
+        // then
+        assertTrue(predicate.test(bean));
+    }
+
     // Test class
     public static class Bean {
 
