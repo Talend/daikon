bfa9efd4d7eee61a58aa0c2a17c506e90f3a9997
fhuaulme <fhuaulme@talend.com>
2020-09-08 16:44:24 +0200

    feat(TDKN-297): Add ability to disable message output
    * Add MDC field to configure whether message go to output or not.
    * Update README documentation with additional information.

--- daikon-audit/audit-common/src/main/java/org/talend/logging/audit/impl/AbstractAuditLoggerBase.java
+++ daikon-audit/audit-common/src/main/java/org/talend/logging/audit/impl/AbstractAuditLoggerBase.java
@@ -1,15 +1,28 @@
 package org.talend.logging.audit.impl;
 
+import java.util.Map;
+
 import org.talend.logging.audit.Context;
 import org.talend.logging.audit.ContextBuilder;
 import org.talend.logging.audit.LogLevel;
-
-import java.util.Map;
 
 /**
  *
  */
 public abstract class AbstractAuditLoggerBase implements AuditLoggerBase {
+
+    /**
+     * <p>
+     * A MDC key to use to customize whether audit logger should output message in audit events. This is interesting
+     * in case only when MDC (Context) contains all the interesting information for the audit ingestion and no message
+     * is required.
+     * </p>
+     * <p>
+     * When set to "false", message in audit event will be replaced by an empty string. It defaults to <code>true</code>
+     * for backward compatibility issues (and also because audit system are generally interested in message).
+     * </p>
+     */
+    public static final String MDC_OUTPUT_MESSAGE = "__output-message__";
 
     private static String formatMessage(String message, Map<String, String> mdcContext) {
         if (mdcContext == null) {
@@ -43,16 +56,20 @@ public abstract class AbstractAuditLoggerBase implements AuditLoggerBase {
 
     private void logInternal(LogLevel level, String category, Context context, Throwable throwable, String message) {
         // creating copy of passed context to be able to modify it
-        Context actualContext = context == null ? ContextBuilder.emptyContext() : ContextBuilder.create(context).build();
+        Context actualContext =
+                context == null ? ContextBuilder.emptyContext() : ContextBuilder.create(context).build();
         Map<String, String> enrichedContext = getEnricher().enrich(category, actualContext);
 
         final AbstractBackend logger = getLogger();
         final Map<String, String> oldContext = logger.getCopyOfContextMap();
         final Map<String, String> completeContext = logger.setNewContext(oldContext, enrichedContext);
         try {
-            final String formattedMessage = formatMessage(message, completeContext);
-
-            logger.log(category, level, formattedMessage, throwable);
+            if (Boolean.parseBoolean(completeContext.getOrDefault(MDC_OUTPUT_MESSAGE, Boolean.TRUE.toString()))) {
+                message = formatMessage(message, completeContext);
+                logger.log(category, level, message, throwable);
+            } else {
+                logger.log(category, level, "", throwable);
+            }
         } finally {
             logger.resetContext(oldContext);
         }
