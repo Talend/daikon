71bee20a7adb5134712c36df885d117612e6725f
fabballe <fabballe@talend.com>
2021-05-18 17:05:12 +0200

    chore(): Jackson deserializer for Talend statistics schema

--- daikon-statistic/src/main/java/org/talend/daikon/statistic/pojo/SimpleStatistic.java
+++ daikon-statistic/src/main/java/org/talend/daikon/statistic/pojo/SimpleStatistic.java
@@ -1,8 +1,5 @@
 package org.talend.daikon.statistic.pojo;
 
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-
-@JsonDeserialize
 public class SimpleStatistic<T> extends Statistic<T> {
 
     private T value;
