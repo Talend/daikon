348110ce5a8bfb42451025ad37255a9a8cc76a71
fabballe <fabballe@talend.com>
2021-05-18 17:31:27 +0200

    chore(): Jackson deserializer for Talend statistics schema

--- daikon-statistic/src/main/java/org/talend/daikon/statistic/deserializer/StatisticDeserializer.java
+++ daikon-statistic/src/main/java/org/talend/daikon/statistic/deserializer/StatisticDeserializer.java
@@ -1,7 +1,6 @@
 package org.talend.daikon.statistic.deserializer;
 
 import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.JsonNode;
